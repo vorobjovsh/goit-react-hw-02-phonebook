9b3791c428249faa158c27884d474f752f5e3d6f
vorobjovsh <vorobjov75@gmail.com>
2022-08-01 11:33:56 +0300

    create step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
+import Filter from './Filter/Filter';
 import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: ''
   }
 
   addName = ({name, number}) => {
@@ -20,8 +27,22 @@ class App extends Component {
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
 
     return (
        <div>
@@ -29,7 +50,8 @@ class App extends Component {
           <ContactForm onSubmit={this.addName} />
 
           <h2>Contacts</h2>
-          <ContactList contacts={contacts} />
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList contacts={visibleContacts} />
        </div>
     );
   }
